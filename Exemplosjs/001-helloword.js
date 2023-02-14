ae89dc1a007f47cc7743f4b0250c742a7471e9e0
Luiz-Rodrigues97 <125319275+Luiz-Rodrigues97@users.noreply.github.com>
2023-02-13 22:50:44 -0300

    Aula Introdução
    criação de Variaveis concatenaçaão
    Tipagem

---
+++ Exemplosjs/001-helloword.js
@@ -0,0 +1,32 @@
+//comentario de uma linha
+console.log('estou desenvolvendo em java-script');
+/*
+comentario de mais de 
+uma linha 
+tres formas de declarar uma variavel em java-script
+(var, let e cont)
+*/
+
+let nome = 'Luiz fernando';
+let idade = 25;
+let trabalho = true;
+let alturapessoa = 1.70
+let coresfavoritas = ['roxo', 'preto', 'azul', 'verde'];
+let endereco = {
+    logradouro : 'Rua',
+    nmlog : 'Brasil',
+    num : '1320',
+    bairro : 'Parque dos Anturios'
+
+ };//objeto
+ let covide = null;
+ let dengue;
+
+
+
+console.log(alturapessoa);
+console.log('nome: ' + nome);
+console.log('0 ' + nome + 'tem ' + idade + ' anos.' );
+console.log('');;
+console.log('--------------------------');
+console.log(typeof(coresfavoritas));
